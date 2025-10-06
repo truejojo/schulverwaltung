322d41fcfe10c2729f8539943a8f42da3bd15c3f
truejojo <johannesveh@yahoo.de>
2025-10-06 08:22:07 +0200

    index.php

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
-<?php
-  echo "Schulverwaltung" 
-?>
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <!-- <title><?= htmlspecialchars($title); ?></title> -->
+  <title>Schulverwaltung</title>
+</head>
+
+<body>
+  <header>
+  </header>
+
+  <main>
+    <h1>Schulverwaltung</h1>
+    <p>Willkommen in der Schulverwaltung!</p>
+  </main>
+
+  <footer>
+
+  </footer>
+</body>
+
+</html>
